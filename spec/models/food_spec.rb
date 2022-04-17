a7172492a7b3f33bfe2a3e37d3654148b2b508ce
Faiz Authar <faizauthar@gmail.com>
2022-04-17 20:27:40 +0700

    refactor food n food_spec to support Factories

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,105 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it 'is valid with a name and a description' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0
-    )
+  it "has a valid factory" do
+    expect(FactoryBot.build(:food)).to be_valid
+  end
 
-    expect(food).to be_valid
+  it 'is valid with a name and a description' do
+    expect(FactoryBot.build(:food)).to be_valid
   end
 
   it 'is invalid without a name' do
-    food = Food.new(
-      name: nil,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0
-    )
-
+    food = FactoryBot.build(:food, name: nil)
     food.valid?
-
     expect(food.errors[:name]).to include("can't be blank")
   end
 
-  it 'is invalid without a description' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: nil,
-      price: 15000.0
-    )
-
-    food.valid?
-
-    expect(food.errors[:description]).to include("can't be blank")
-  end
-
   it "is invalid with a duplicate name" do
-    food1 = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-    )
-    
-    food2 = Food.new(
-      name: "Nasi Uduk",
-      description: "Just with a different description.",
-      price: 10000.0
-    )
+    food1 = FactoryBot.create(:food, name: 'Nasi Uduk')
+    food2 = FactoryBot.build(:food, name: 'Nasi Uduk')
 
     food2.valid?
-    
+
     expect(food2.errors[:name]).to include("has already been taken")
   end
 
   describe 'self#by_letter' do
-    it "should return a sorted array of results that match" do
-      food1 = Food.create(
-        name: "Nasi Uduk",
-        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0
-      )
-
-      food2 = Food.create(
-        name: "Kerak Telor",
-        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
-        price: 8000.0
-      )
-
-      food3 = Food.create(
-        name: "Nasi Semur Jengkol",
-        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
-        price: 8000.0
-      )
-
-      expect(Food.by_letter("N")).to eq([food3, food1])
+    context 'with matching letter' do
+      it "should return a sorted array of results that match" do
+        food1 = FactoryBot.create(:food, name: 'Nasi Uduk')
+        food2 = FactoryBot.create(:food, name: 'Kerak Telor')
+        food3 = FactoryBot.create(:food, name: 'Nasi Semur Jengkol')
+  
+        expect(Food.by_letter("N")).to eq([food3, food1])
+      end
     end
   end
 
-  describe 'Homework' do
-    it "is invalid with non-numeric value for price field" do
-      food = Food.new(
-        name: "Nasi Goreng",
-        description: 'The superior race of Nasi variant!',
-        price: "lima belas ribus"
-      )
+  # describe 'Homework' do
+  #   it "is invalid with non-numeric value for price field" do
+  #     food = FactoryBot.create(:food, name: 'Nasi Goreng', description: 'The superior race of Nasi variant!', price: "lima belas ribus")
   
-      food.valid?
+  #     food.valid?
   
-      expect(food.errors[:price]).to include("must be numeric")
-    end
+  #     expect(food.errors[:price]).to include("must be numeric")
+  #   end
 
-    it 'is invalid if price less than 0.01' do
-      food = Food.new(
-        name: "Nasi Goreng",
-        description: 'The superior race of Nasi variant!',
-        price: 0.001
-      )
+  #   it 'is invalid if price less than 0.01' do
+  #     food = FactoryBot.create(:food, name: 'Nasi Goreng', description: 'The superior race of Nasi variant!', price: 0.001)
   
-      food.valid?
+  #     food.valid?
   
-      expect(food.errors[:price]).to include("must more than 0.01")
-    end
-  end
+  #     expect(food.errors[:price]).to include("must more than 0.01")
+  #   end
+  # end
 end
